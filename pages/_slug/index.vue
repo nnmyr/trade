cd232553bccda503b935afd5bb0a8d3860b87d3f
二宮 <ninomiya@agentplus.co.jp>
2021-09-19 15:57:42 +0900

    【add】詳細ページ追加

---
+++ pages/_slug/index.vue
@@ -0,0 +1,67 @@
+<template>
+  <main class="main">
+    <h1 class="title">{{ title }}</h1>
+    <p class="publishedAt">{{ publishedAt }}</p>
+    <p class="category">{{ category && category.name }}</p>
+    <div class="post" v-html="body"></div>
+  </main>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async asyncData({ params }) {
+    const { data } = await axios.get(
+      `https://trade.microcms.io/api/v1/blog/${params.slug}`,
+      {
+        headers: {'X-API-KEY':'8ccac964-e916-4075-ade4-0435d219c3b5'}
+      }
+    )
+    return data
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.main {
+  width: 960px;
+  margin: 0 auto;
+}
+
+.title {
+  margin-bottom: 20px;
+}
+
+.publishedAt {
+  margin-bottom: 40px;
+}
+
+.post {
+  & > h1 {
+    font-size: 30px;
+    font-weight: bold;
+    margin: 40px 0 20px;
+    background-color: #eee;
+    padding: 10px 20px;
+    border-radius: 5px;
+  }
+
+  & > h2 {
+    font-size: 24px;
+    font-weight: bold;
+    margin: 40px 0 16px;
+    border-bottom: 1px solid #ddd;
+  }
+
+  & > p {
+    line-height: 1.8;
+    letter-spacing: 0.2px;
+  }
+
+  & > ol {
+    list-style-type: decimal;
+    list-style-position: inside;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,24 @@
 <template>
-  <Tutorial/>
+  <ul>
+    <li v-for="content in contents" :key="content.id">
+      <nuxt-link :to="`/${content.id}`">
+        {{ content.title }}
+      </nuxt-link>
+    </li>
+  </ul>
 </template>
 
 <script>
-export default {}
+import axios from 'axios'
+export default {
+  async asyncData() {
+    const { data } = await axios.get(
+      'https://trade.microcms.io/api/v1/blog',
+      {
+        headers: {'X-API-KEY':'8ccac964-e916-4075-ade4-0435d219c3b5'}
+      }
+    )
+    return data
+  }
+}
 </script>
