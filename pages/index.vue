a7cb9961b652c39f3d082456650a9628323e651d
二宮 <ninomiya@agentplus.co.jp>
2021-12-27 17:32:44 +0900

    【update】一覧ページに公開日追加し、出力件数を1000に変更

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <ul>
     <li v-for="content in contents" :key="content.id">
+      <time>{{ content.publishedAt }}</time>
       <nuxt-link :to="`/${content.id}`">
         {{ content.title }}
       </nuxt-link>
@@ -11,12 +12,25 @@
 <script>
 // microCMSからコンテンツを取得
 import axios from 'axios'
+
+// publishedAtを日本時間に変換
+import dayjs from 'dayjs'
+import utc from 'dayjs/plugin/utc'
+import timezone from 'dayjs/plugin/timezone'
+import 'dayjs/locale/ja'
+dayjs.extend(utc)
+dayjs.extend(timezone)
+dayjs.locale('ja')
+
 export default {
   async asyncData() {
     const { data } = await axios.get(
-      'https://trade.microcms.io/api/v1/blog',
+      'https://trade.microcms.io/api/v1/blog?limit=1000',
       {headers: {'X-API-KEY':'8ccac964-e916-4075-ade4-0435d219c3b5'}}
     )
+    data.contents.forEach((key) => {
+      key['publishedAt'] = dayjs.utc(key['publishedAt']).tz('Asia/Tokyo').format('YYYY/MM/DD(ddd)')
+    })
     return data
   }
 }
